675c52b15f50e7714272ec7a9448660e29082702
Victor Hugo Segundo Aguilar <victorhsegundoa@gmail.com>
2026-07-06 12:21:32 -0600

    [UPDATE] - Addings req.params

--- src/modules/products/product.routes.ts
+++ src/modules/products/product.routes.ts
@@ -10,3 +10,18 @@ productRoutes.get('/', (_request, response) => {
     items: products
   });
 });
+
+productRoutes.get('/:id', (request, response) => {
+  const { id } = request.params;
+
+  const product = products.find((currentProduct) => currentProduct.id === id);
+
+  if (!product) {
+    response.status(404).json({
+      message: 'Product not found'
+    });
+    return;
+  }
+
+  response.status(200).json(product);
+});
